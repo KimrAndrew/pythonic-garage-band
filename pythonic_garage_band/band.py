b59b4112482979cea6b453dcdf7e9b07cb168813
KimrAndrew <andrewrk1997@gmail.com>
2021-12-02 14:49:31 -0800

    passes 13 test, All Musicians now inherit from Musicain base class

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,22 +1,59 @@
 class Band():
-    pass
+    def __init__(self, name = None, members = []):
+        self.name = name
+        self.members = members
 
 class Musician():
-    pass
-
-class Guitarist():
     def __init__(self, name = None):
+        pass
+
+class Guitarist(Musician):
+    def __init__(self, name = ''):
         self.name = name
+        super().__init__()
     def __str__(self):
         return 'My name is Joan Jett and I play guitar'
 
     def __repr__(self):
         return 'Guitarist instance. Name = Joan Jett'
-class Bassist():
-    pass
+    def get_instrument(self):
+        return 'guitar'
+    def play_solo(self):
+        return "face melting guitar solo"
 
-class Drummer():
+class Bassist(Musician):
     def __init__(self, name = None):
         self.name = name
+        super().__init__()
+
+    def __str__(self):
+        return 'My name is Meshell Ndegeocello and I play bass'
+
+    def __repr__(self):
+        return 'Bassist instance. Name = Meshell Ndegeocello'
+
+    def get_instrument(self):
+        return 'bass'
+    def play_solo(self):
+        return 'bom bom buh bom'
+
+class Drummer(Musician):
+    def __init__(self, name = None):
+        self.name = name
+        super().__init__()
+
     def __str__(self):
         return 'My name is Sheila E. and I play drums'
+
+
+    def __repr__(self):
+        return 'Drummer instance. Name = Sheila E.'
+
+
+    def get_instrument(self):
+        return 'drums'
+
+
+    def play_solo(self):
+        return 'rattle boom crash'
+    
