82573309b34266dca27fd646b4f5288b8bbc66bc
KimrAndrew <andrewrk1997@gmail.com>
2021-12-02 13:52:40 -0800

    passes first test

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -5,8 +5,10 @@ class Musician():
     pass
 
 class Guitarist():
-    pass
-
+    def __init__(self, name = None):
+        self.name = name
+    def __str__(self):
+        return 'My name is Joan Jett and I play guitar'
 class Bassist():
     pass
 
