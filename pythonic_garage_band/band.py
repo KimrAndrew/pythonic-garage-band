28092110fdb5aaeb83768c3ffec2bf431bee089d
KimrAndrew <andrewrk1997@gmail.com>
2021-12-02 18:24:34 -0800

    all tests pass, refactored to use less hard coding, acknowledged the existance of guitarists other than Joan Jett

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,57 +1,71 @@
 class Band():
+
+    instances = []
+
     def __init__(self, name = None, members = []):
         self.name = name
         self.members = members
+        Band.instances.append(self)
+
+    def __eq__(self, other):
+        return self.name == other.name and self.members == other.members
+    def to_list(self):
+        return self.members
+
+    def play_solos(self):
+        solos = []
+        for member in self.members:
+            solos.append(member.play_solo())
+        return solos
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
 
 class Musician():
-    def __init__(self, name = None):
-        pass
+    def __init__(self, name = None, instrument = None):
+        self.name = name
+        self.instrument = instrument
+    def __str__(self):
+        return f'My name is {self.name} and I play {self.instrument}'
 
 class Guitarist(Musician):
-    def __init__(self, name = ''):
-        self.name = name
-        super().__init__()
-    def __str__(self):
-        return 'My name is Joan Jett and I play guitar'
+
+    def __init__(self, name=None, instrument='guitar'):
+        super().__init__(name=name, instrument=instrument)
 
     def __repr__(self):
-        return 'Guitarist instance. Name = Joan Jett'
+        return f'Guitarist instance. Name = {self.name}'
     def get_instrument(self):
-        return 'guitar'
+        return self.instrument
     def play_solo(self):
         return "face melting guitar solo"
 
 class Bassist(Musician):
-    def __init__(self, name = None):
-        self.name = name
-        super().__init__()
 
-    def __str__(self):
-        return 'My name is Meshell Ndegeocello and I play bass'
+    def __init__(self, name=None, instrument='bass'):
+        super().__init__(name=name, instrument=instrument)
 
     def __repr__(self):
-        return 'Bassist instance. Name = Meshell Ndegeocello'
+        return f'Bassist instance. Name = {self.name}'
 
     def get_instrument(self):
-        return 'bass'
+        return self.instrument
     def play_solo(self):
         return 'bom bom buh bom'
 
 class Drummer(Musician):
-    def __init__(self, name = None):
-        self.name = name
-        super().__init__()
 
-    def __str__(self):
-        return 'My name is Sheila E. and I play drums'
+    def __init__(self, name=None, instrument='drums'):
+        super().__init__(name=name, instrument=instrument)
 
 
     def __repr__(self):
-        return 'Drummer instance. Name = Sheila E.'
+        return f'Drummer instance. Name = {self.name}'
 
 
     def get_instrument(self):
-        return 'drums'
+        return self.instrument
 
 
     def play_solo(self):
